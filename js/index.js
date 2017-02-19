952f3a83bdaf219a2f881572c0159bfdb476f921
Robert Spurlin <rts3e@mtmail.mtsu.edu>
2017-02-18 23:05:15 -0600

    Color changes, quotation removals

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	
 	// Setup definitions
-
     $(window).resize(function() {
         var homeHeight = $(this).height();
         var hCenter = (($(this).height() /2) - ($("#box").height() /2));
@@ -9,16 +8,36 @@ $(document).ready(function() {
         $("#box").css("margin-top", hCenter);
     }).resize();
 
-    // API Integration. Credit to Chris Coyier https://quotesondesign.com
-
+    // API Integration. Credit to Forismatic https://forismatic.com
     $("#quote-button").on("click", function() {
     	$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?").done(update).fail(error);
+
+    	// Random Color Generator
+    	var letters = '0123456789ABCDEF';
+    	var randomColor = '#';
+    	for (var i = 0; i < 6; i++ ) {
+        	randomColor += letters[Math.floor(Math.random() * 16)];
+   	 	}
+  		
+  		$(".color-change").css("color", randomColor);
+  		$("#background").css("background-color", randomColor);
+
     });
 
     function update(response) {
-    	$("#quote").html(
-    		JSON.stringify(response.quoteText) + "<br />" + JSON.stringify(response.quoteAuthor)
-    		);
+    	var quote = JSON.stringify(response.quoteText) + "<br />";
+    	quote = quote.replace(/"/g, '');
+
+    	var author = "-" + JSON.stringify(response.quoteAuthor);
+    	author = author.replace(/"/g, '');
+    	
+    	if (author == "-") {
+    		author += "Unknown";
+    	}
+
+    	var html = quote + author;
+
+    	$("#quote").html(html);
     }
 
     function error() {
